bf2a911fdd9671059e2e631edb5b64146e74023a
Bryan <bryan.soares19@hotmail.com>
2022-09-05 21:32:47 -0300

    php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
 
-$notas_dos_alunos = [
-    '7.4', '1.7', '8.5', '3.5', '4.4', '8.7', '6.4', '8.4', '1.2', '4.3', '9.8', '0.5', '8.2',
-    '4.7', '1.1', '3.3', '3.4', '4.8', '8.7', '5.4', '2.2', '3.7', '5.9', '7.4', '4.8', '4.7', '1.5', '8.4', '2.1', '2.7'];
-
-
-echo "aqui vai o foreach";
-
-foreach ($notas_dos_alunos as $k => $i) {
-    if($notas_dos_alunos[$k] <= 6.0){
-        echo "Aprovado <br>"."indice". $k ."conteudo".$i;
-    }else{
-        echo "Reprovado <br>"."indice". $k ."conteudo".$i;
-    };
+function p($number, $potencia){
+    $i = 1;
+    $resultado = 1;
+    do{
+        $i++;
+        $resultado *= $number;
+   }while($i <= $potencia);
+   return $resultado;
 }
+echo p(2,3);
