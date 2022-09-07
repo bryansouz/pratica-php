a04d425e4a9f259e6df3f852f4dd03599a5616d7
Bryan <bryan.soares19@hotmail.com>
2022-09-06 23:34:41 -0300

    form

--- index.php
+++ index.php
@@ -1,12 +1,68 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function p($number, $potencia){
-    $i = 1;
-    $resultado = 1;
-    do{
-        $i++;
-        $resultado *= $number;
-   }while($i <= $potencia);
-   return $resultado;
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=g, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .error{
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <a href="./enviar.php">enviar</a>
+    <form action="" method="post">
+        Nome: <input type="text" name="nome">
+
+        <br>
+        <br>
+        Email: <input type="text" name="email">
+
+        <br>
+        <br>
+        Genero:
+        <input type="radio" value="Masculino" name="genero" checked> Masculino
+        <input type="radio" value="Feminino" name="genero"> Feminino
+        <input type="radio" value="Outros" name="genero"> Outros
+
+
+
+        <button name="enviado" type="submit">Enviar</button>
+    </form>
+
+    <h1>Dados enviados</h1>
+    <?php
+
+
+    if(isset($_POST["enviado"])){
+    echo "Seu nome é: " . $_POST["nome"];
+    echo "<br><br>";
+    if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false){
+        echo "<p class='error'>Preencha o campo email </p>";
+    }else{ 
+        echo "Seu email é: ".$_POST["email"];
+    };
+    echo "<br><br>";
+    echo "Genero: " . $_POST["genero"];
+
 }
-echo p(2,3);
+    ?>
+
+    <script>
+
+        let inp = document.querySelectorAll("input");
+        let body = document.getElementsByName("body");
+        console.log(body);
+        console.log(inp);
+        inp.forEach(e =>{
+            e.required = true
+          
+        })
+    </script>
+</body>
+
+</html>
